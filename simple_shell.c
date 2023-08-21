782702c27c843e05693658e65f4bb4ab31637056
Glozano26 <lozsuperbia2014@gmail.com>
2023-08-21 16:00:15 -0700

    found path

--- simple_shell.c
+++ simple_shell.c
@@ -41,52 +41,54 @@ int main()
         	{
 			snprintf(command_path, sizeof(command_path), "%s/%s", token, args[0]);
 
-		if (access(command_path, X_OK) == 0)
-		{
-                	child_pid = fork();
-
-			/* Elimina el salto de línea final */
-			if (line_read > 0 && line[line_read - 1] == '\n')
+			if (access(command_path, X_OK) == 0)
 			{
-                        	line[line_read - 1] = '\0';
-                	}
-			/* val si es solo una linea de espacios */
-                	for (i = 0; line[i] != '\0'; i++)
-			{
-                        	if (line[i] != ' ')
-                                break;
-             		}
-			if (line[i] == '\0')
-			{/*solo se cumple si es una cadena de espacios*/
-                        	continue;
                 		child_pid = fork();
-			}
-			if (child_pid == -1)
-                	{
-                        	perror("Fork failed");
+
+				/* Elimina el salto de línea final */
+				if (line_read > 0 && line[line_read - 1] == '\n')
+				{
+                        		line[line_read - 1] = '\0';
+                		}
+				/* val si es solo una linea de espacios */
+                		for (i = 0; line[i] != '\0'; i++)
+				{
+                        		if (line[i] != ' ')
+					break;
+             			}
+				if (line[i] == '\0')
+				{/*solo se cumple si es una cadena de espacios*/
+                        		continue;
+                			child_pid = fork();
+				}
+				if (child_pid == -1)
+                		{
+                        		perror("Fork failed");
+                        		exit(1);
+               			}
+				else if (child_pid == 0)
+				{ /* Código del proceso hijo */
+                        	/* Analiza la línea de comandos en argumentos */
+                        		int i = 0;
+                        		args[i] = strtok(line, " ");
+				}
+                        	while (args[i] != NULL)
+				{
+                                	i++;
+                                	args[i] = strtok(NULL, " ");
+				}
+                        	args[i] = NULL; /* Termina la lista de argumentos */
+                        	/* Ejecuta el comando */
+                        	execve(args[0], args, NULL);
+                        	perror("./shell");
                         	exit(1);
-               		}
-			else if (child_pid == 0)
-			{ /* Código del proceso hijo */
-                        /* Analiza la línea de comandos en argumentos */
-                        	int i = 0;
-                        	args[i] = strtok(line, " ");
-                        while (args[i] != NULL)
-			{
-                                i++;
-                                args[i] = strtok(NULL, " ");
-                        }
-                        args[i] = NULL; /* Termina la lista de argumentos */
-                        /* Ejecuta el comando */
-                        execve(args[0], args, NULL);
-                        perror("./shell");
-                        exit(1);
-                	}
-                	else
-                	{
-                        	wait(&status);
-				break;
-                	}
+                		else
+                		{
+                        		wait(&status);
+					break;
+                		}
+			}
+			token = strtok(NULL, ":");
 		}	
         }
         return (0);
