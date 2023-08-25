72697454291eec212da4df5da07a490551159adb
Glozano26 <lozsuperbia2014@gmail.com>
2023-08-25 09:54:47 -0700

    final proyect

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+extern char **environ;
+
+
+
+#endif
